904059940e5f43d0af69f3bebc6076a6e8149319
Nikhil Sethi <132539777+Nikhilsethi7668@users.noreply.github.com>
2024-05-11 16:15:17 +0530

    Update Context.jsx

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -5,22 +5,23 @@ export const productcontext = createContext();
 
 const Context = (props) => {
     // now we are using local storage instead of api so that new product can be added in the data
-    const [product, setproduct] = useState(JSON.parse(localStorage.getItem('products')) || null);
-    // const getproducts = async () => {
-    //     try {
-    //         const { data } = await axios("/products");
-    //         // console.log(data)
-    //         setproduct(data);
+    // const [product, setproduct] = useState(JSON.parse(localStorage.getItem('products')) || null);
+    const [product, setproduct] = useState()
+    const getproducts = async () => {
+        try {
+            const { data } = await axios("/products");
+            // console.log(data)
+            setproduct(data);
 
 
-    //     } catch (error) {
-    //         console.log(error);
+        } catch (error) {
+            console.log(error);
 
-    //     }
-    // }
-    // useEffect(() => {
-    //     getproducts();
-    // }, []);
+        }
+    }
+    useEffect(() => {
+        getproducts();
+    }, []);
     console.log(product);
 
     return (
